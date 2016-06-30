78ff930bb9601dee0841b0291586c6b4d47a3dc2
Arthur Bedder III <arthurbedder3@gmail.com>
2016-06-30 16:16:18 -0400

    updated parser

--- catalogParsers.py
+++ catalogParsers.py
@@ -4,16 +4,16 @@ def parseCoffeeNetToScotts(jsonData):
     ScottsCatalog = []
     newItem = {}
     for item in jsonData:
-        newItem['name'] = item['description']
-        newItem['description'] = item['tastingNotes']
-        newItem['size'] = item['pricingData'][0]['size']
-        newItem['price'] = item['pricingData'][0]['cost']
-        newItem['quantity'] = item['pricingData'][0]['inStock']
-        newItem['fairTrade'] = ""
-        newItem['origin'] = ""
-        newItem['grind'] = ""
-        ScottsCatalog.append(newItem)
-
+        for priceData in item['pricingData']:
+            newItem['name'] = item['description']
+            newItem['description'] = item['tastingNotes']
+            newItem['size'] = priceData['size']
+            newItem['price'] = priceData['cost']
+            newItem['quantity'] = priceData['inStock']
+            newItem['fairTrade'] = ""
+            newItem['origin'] = ""
+            newItem['grind'] = ""
+            ScottsCatalog.append(newItem)
 
     return ScottsCatalog
 
@@ -23,5 +23,5 @@ def callSupplier(supplierURL):
 
 
 coffeenet = "http://coffee-net.azurewebsites.net/api/catalog/coffee"
-newCatalog = parseCoffeeNetToScotts(callSupplier(coffeenet))
+newCatalog = parseCoffeeNetToScotts2(callSupplier(coffeenet))
 print newCatalog
